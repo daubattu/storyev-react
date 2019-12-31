398e0f60736622485d8e4129c6a8c543f28a3aab
Nguyễn Hưng Khánh <nhk020996@gmail.com>
2019-12-31 16:52:21 +0700

    add travis setup

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-set -xe
+echo "Hello world!"
+# set -xe
 
-if [ $TRAVIS_BRANCH == 'master' ] ; then
-  eval "$(ssh-agent -s)"
-  ssh-add
-  npm run build
-  rsync -rq --delete --rsync-path="mkdir -p react-app && rsync" \
-  $TRAVIS_BUILD_DIR/public travis@<ip>:react-app
-else
-  echo "Not deploying, since this branch isn't master."
-fi
+# if [ $TRAVIS_BRANCH == 'master' ] ; then
+#   eval "$(ssh-agent -s)"
+#   ssh-add
+#   npm run build
+#   rsync -rq --delete --rsync-path="mkdir -p react-app && rsync" \
+#   $TRAVIS_BUILD_DIR/public travis@<ip>:react-app
+# else
+#   echo "Not deploying, since this branch isn't master."
+# fi
